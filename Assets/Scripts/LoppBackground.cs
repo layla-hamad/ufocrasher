9af1f98cfc632c3c93eca0d9f987557c1b92f674
Layla Hamad <laayla.hamad@gmail.com>
2020-02-13 15:10:49 +0100

    added background

---
+++ Assets/Scripts/LoppBackground.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class LoppBackground : MonoBehaviour
+{
+    public float speed = 0.5f;
+    private float offset = 0f;
+    private Renderer renderer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        renderer = GetComponent<Renderer>();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        offset = Mathf.Repeat(offset + Time.deltaTime * speed, 1f);
+        renderer.sharedMaterial.SetTextureOffset("_MainTex", new Vector2(offset, 0));
+    }
+}
