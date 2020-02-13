a21a2583f72740be5fbdbeb36f55d00c645b6fd5
Layla Hamad <laayla.hamad@gmail.com>
2020-02-13 14:31:10 +0100

    added ufo, skyscraper

---
+++ Assets/Scripts/UFO.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UFO : MonoBehaviour
+{
+    public float jumpforce = 400f;
+    private Rigidbody2D rigidbody;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rigidbody.AddForce(Vector2.up * jumpforce);
+        }
+    }
+}
