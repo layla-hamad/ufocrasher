9e2fcdb5dd27bf21a2ff49c1cfb9abfc0bb4aa2f
Layla Hamad <laayla.hamad@gmail.com>
2020-02-25 00:13:49 +0100

    Added Skyscraper Spawner and Skyscraper Destroyer

---
+++ Assets/Scripts/Skyscraper.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Skyscraper : MonoBehaviour
+{
+    public float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        /*
+         * Moves the object to left with the factor speed, normalized 
+         * with the deltaTime (time between the last frame and this frame)
+         */
+        transform.position += Vector3.left * speed * Time.deltaTime;
+    }
+}
---
+++ Assets/Scripts/SkyscraperSpawner.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkyscraperSpawner : MonoBehaviour
+{
+    public GameObject skyscraper;
+    public float deltaSpawnTimeMin;
+    public float deltaSpawnTimeMax;
+
+    public float deltaHeight;
+
+    private float nextSpawnTime;
+    // Start is called before the first frame update
+    void Start()
+    {
+        nextSpawnTime = Time.time + GetRandomTimeDelta();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //When the delta spawn time was awaited
+        if (Time.time > nextSpawnTime)
+        {
+            //set the next spawn time to a time in the future
+            nextSpawnTime = Time.time + GetRandomTimeDelta();
+
+            //spawn a new skyscraper at a random position with no rotation
+            Instantiate(skyscraper, GetRandomPos(), Quaternion.identity);
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawLine(transform.position - new Vector3(0.5f, deltaHeight), transform.position - new Vector3(-0.5f, deltaHeight));
+        Gizmos.DrawLine(transform.position + new Vector3(0.5f, deltaHeight), transform.position + new Vector3(-0.5f, deltaHeight));
+
+    }
+
+    private float GetRandomTimeDelta()
+    {
+        return Random.Range(deltaSpawnTimeMin, deltaSpawnTimeMax);
+    }
+
+    private Vector2 GetRandomPos()
+    {
+        var y = Random.Range(transform.position.y - deltaHeight, transform.position.y + deltaHeight);
+        return new Vector2(transform.position.x, y);
+    }
+}
