f51ab1b2d8638fb0f62d7a359e9e26b5b26193e1
Layla Hamad <laayla.hamad@gmail.com>
2020-02-25 23:30:40 +0100

    Added boundaries, lives and invincibility after having taken damage

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameController : MonoBehaviour
+{
+    private static GameController instance;
+    public static GameController Instance => instance;
+
+
+    private void Awake()
+    {
+        //if already an instance exists destroy this gameobject
+        if (instance != null)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            //make this object a singleton
+            instance = this;
+            //do not destroy this object on scene switch
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+
+    public void LoadGame()
+    {
+        SceneManager.LoadScene("GameScene");
+    }
+
+    public void LoadMenu()
+    {
+        SceneManager.LoadScene("MenuScene");
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/Scripts/UFO.cs
+++ Assets/Scripts/UFO.cs
@@ -4,12 +4,55 @@ using UnityEngine;
 
 public class UFO : MonoBehaviour
 {
+    /// <summary>
+    /// Upwards force applied to the UFOs rigidbody when jump is pressed
+    /// </summary>
     public float jumpforce = 400f;
+
+    /// <summary>
+    /// The period of time in which UFO is invincible after having taken damage
+    /// </summary>
+    public float deltaInvicibleTime = 2f;
+
+    /// <summary>
+    /// The color the UFO has after having taken damage
+    /// </summary>
+    public Color damageColor;
+
+    /// <summary>
+    /// The rigidbody controls the physics of this object
+    /// </summary>
     private Rigidbody2D rigidbody;
+/// <summary>
+///The renderer controls the appearance of this object
+/// </summary>
+    private Renderer render; 
+
+    private int life = 3;
+    private float timeUntilVunerable = 0f;
+
+    /// <summary>
+    /// The life of the UFO: when zero, UFO dies
+    /// </summary>
+    private int Life
+    {
+        get => life;
+        set
+        {
+            life = value;
+            if(life <= 0)
+            {
+                Die();
+            }
+        }
+    }
+
     // Start is called before the first frame update
     void Start()
     {
+        // get the needed components of this gameobject
         rigidbody = GetComponent<Rigidbody2D>();
+        render = GetComponent<Renderer>();
     }
 
     // Update is called once per frame
@@ -19,5 +62,39 @@ public class UFO : MonoBehaviour
         {
             rigidbody.AddForce(Vector2.up * jumpforce);
         }
+
+
+    }
+
+    /// <summary>
+    /// Damage the UFO, the UFO loses one life and is invicible for a short time.
+    /// </summary>
+    public void Damage()
+    {
+        if(Time.time > timeUntilVunerable)
+        { 
+            Life--;
+            timeUntilVunerable = Time.time + deltaInvicibleTime;
+            render.material.color = damageColor;
+            // Invoke the method RevertDamageColor() after deltaInvincibleTime
+            Invoke(nameof(RevertDamageColor), deltaInvicibleTime);
+        }
+ 
+    }
+
+
+    private void RevertDamageColor()
+    {
+        render.material.color = Color.white;
+    }
+
+
+    /// <summary>
+    /// Destroys the UFO and ends the game
+    /// </summary>
+    public void Die()
+    {
+        Destroy(gameObject);
+        GameController.Instance.LoadMenu();
     }
 }
